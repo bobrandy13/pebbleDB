1d67e90d6b0cdd921cc8dd814e6382bb9f500f18
Kevin <Kevin@DESKTOP-HMGQPA7>
2024-12-17 17:11:31 +1100

    Add project files.

---
+++ Core/Core/Database.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Core
+{
+    public class Database
+    {
+        private readonly Dictionary<string, Table> _tables;
+        // constructor
+        public Database(Config config)
+        {
+            _tables = []; 
+        }
+
+        public void createTable(string tableName, List<Schema> columns)
+        {
+            // for each column, we also need the size  
+
+            if (_tables.ContainsKey(tableName))
+            {
+                throw new InvalidOperationException("Table already exists");
+            }
+
+            _tables[tableName] = new Table(tableName, columns); 
+        }
+
+	public void updateTable(string tableName, List<Schema> updatedColumns) 
+	{
+		if (!_tables.ContainsKey(tableName)) 
+		{
+			throw new KeyNotFoundException("Table does not exist"); 
+		}
+	}
+
+	public void deleteTable(string tableName) 
+	{
+		if (!_tables.ContainsKey(tableName)) 
+		{
+			throw new KeyNotFoundException("Table does not exist"); 
+		}
+
+		_tables.Remove(tableName); 
+	}
+
+	public void clearDatabase() {
+		_tables.Clear(); 
+	}
+
+        public void outputTableFormat(string tableName)
+        {
+            if (!_tables.ContainsKey(tableName))
+            {
+                throw new KeyNotFoundException("Table does not exist");
+            }
+
+        }
+
+        public void outputTableData(string tableName)
+        {
+            if (!_tables.ContainsKey(tableName))
+            {
+                throw new KeyNotFoundException("Table does not exist");
+            }
+        }
+
+    }
+
+  
+    
+    public class Schema
+    {
+        public required string ColumnName; 
+        public required long byteSize;
+    }
+
+    public class Parameters 
+    {
+    }
+
+    public class Config 
+    {
+    }
+}
---
+++ Core/Core/Table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Core;
+
+public class Table
+{
+	public Table(string tableName, List<Schema> columnNames) 
+	{
+	}
+
+	public int Insert() 
+	{
+	}
+
+	public int Update() 
+	{
+	}
+
+	public int Delete() 
+	{
+	}
+
+	public List<Schema> Query(Parameters parameters)
+	{
+
+		// Querying the data should return one or more rows; 
+	}
+
+}
