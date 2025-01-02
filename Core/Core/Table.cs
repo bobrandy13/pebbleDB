5cdc196c756ce8af9a0cb50fc11fc239b35a9edf
kevinhuang <khuang891@gmail.com>
2025-01-03 10:35:25 +1100

    changed btree to self balancing tree

--- Core/Core/Table.cs
+++ Core/Core/Table.cs
@@ -3,94 +3,101 @@ using Core;
 
 public class Table
 {
-	protected Schema schema; 
+    protected Schema schema;
     private readonly B_Tree = new B_Tree();
 
-	public Table(string tableName, Schema columnNames) 
-	{
-		this.schema = columnNames; 
+    public Table(string tableName, Schema columnNames)
+    {
+        this.schema = columnNames;
 
-		// TODO: Create 2 files
-		// 1. Table Index file. 
-		// 2. Data file. Binary data only
-	}
+        // TODO: Create 2 files
+        // 1. Table Index file. 
+        // 2. Data file. Binary data only
+    }
 
-	// TODO: 
-	// This should do two things.
-	// 1. Insert into primary and secondary Index
-	// 2 .This should also append to the binary data file. 
-	public int Insert(Schema Item) 
-	{
+    // TODO: 
+    // This should do two things.
+    // 1. Insert into primary and secondary Index
+    // 2 .This should also append to the binary data file. 
+    public int Insert(Schema Item)
+    {
 
-		return 0; 
-	}
+        return 0;
+    }
 
-	// TODO: 
-	// This can just be an abstraction upon the Insert Function(); 
-	//
-	// Returns 0 if there are no issues; 
-	public int InsertMany(List<Schema> items) 
-	{
+    // TODO: 
+    // This can just be an abstraction upon the Insert Function(); 
+    //
+    // Returns 0 if there are no issues; 
+    public int InsertMany(List<Schema> items)
+    {
 
-		// TODO: Make this more efficient; 
-		foreach (var item in items) {
-			try {
-				this.Insert(item); 
-			} catch (Exception e) {
-				throw new Exception("Insert failed", e); 
-			}
-		}
-		return 0; 
-	}
+        // TODO: Make this more efficient; 
+        foreach (var item in items)
+        {
+            try
+            {
+                this.Insert(item);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Insert failed", e);
+            }
+        }
+        return 0;
+    }
 
-	public int Update(string id, Schema updatedSchema)  
-	{
-		// figure out the index, and then change the binary data. 
+    public int Update(string id, Schema updatedSchema)
+    {
+        // figure out the index, and then change the binary data. 
 
-		return 0; 
-	}
+        return 0;
+    }
 
-	public int Delete() 
-	{
-		// should find the index, and then delete it from the file 
+    public int Delete()
+    {
+        // should find the index, and then delete it from the file 
 
-		return 0; 
-	}
+        return 0;
+    }
 
-	public Schema GetSchema() 
-	{
-		if (this.schema == null) {
-			throw new NullReferenceException("No pre-existing schema defined."); 
-		}
-		return this.schema;
-	}
+    public Schema GetSchema()
+    {
+        if (this.schema == null)
+        {
+            throw new NullReferenceException("No pre-existing schema defined.");
+        }
+        return this.schema;
+    }
 
-	public void UpdateSchema(Schema newSchema) {
-		this.schema = newSchema;
-	}
+    public void UpdateSchema(Schema newSchema)
+    {
+        this.schema = newSchema;
+    }
 
-	// =========================
-	// ======== QUERYING =======
-	// =========================
+    // =========================
+    // ======== QUERYING =======
+    // =========================
 
-	// Summary: The id is going to be the hashed unique field of the database 
-	public Schema Select(string id) 
-	{
-		var data = new byte[256]; 
+    // Summary: The id is going to be the hashed unique field of the database 
+    public Schema Select(string id)
+    {
+        var data = new byte[256];
 
-		using (var reader = new BinaryReader(new FileStream("hello world", FileMode.Open))) {
-			reader.BaseStream.Seek(50, SeekOrigin.Begin); 
-		}
+        using (var reader = new BinaryReader(new FileStream("hello world", FileMode.Open)))
+        {
+            reader.BaseStream.Seek(50, SeekOrigin.Begin);
+        }
 
 
         // TODO: Find the ID within the index hashtable and then find the corresponding bytes 
-        return new Schema("", []); 
-	}
+        return new Schema("", []);
+    }
 
-	public List<Schema> SelectMany(List<string> id) 
-	{
-		return []; 
-	}
+    public List<Schema> SelectMany(List<string> id)
+    {
+        return [];
+    }
 
 
 
---
+++ Datastructures/Datastructures/AVLTree.cs
@@ -0,0 +1,136 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Transactions;
+
+namespace Datastructures
+{
+
+    public class Node(Node? left, Node? right, long key, long value, bool isLeaf)
+    {
+        public bool IsLeaf = isLeaf;
+        public long Key = key;
+        public long Value = value;
+
+
+        public Node? Left = left;
+        public Node? Right = right;
+    }
+
+    public class Tree
+    {
+        private int GetBalanceFactor(Node? node)
+        {
+
+            if (node == null) return 0;
+
+            return GetBalanceFactor(node.Left) - GetBalanceFactor(node.Right);
+
+        }
+
+        public Node Insert(ref Node? root, long key, long value)
+        {
+            if (root == null)
+            {
+                return new Node(null, null, key, value, true);
+            }
+            else if (root.Key < key)
+            {
+                root.Left = Insert(ref root.Left, key, value);
+            }
+            else
+            {
+                root.Right = Insert(ref root.Right, key, value);
+            }
+
+            // Perform balancing operations 
+
+            var balance_factor = this.GetBalanceFactor(root);
+
+            if (balance_factor < -1)
+            {
+                // left leaning
+                if (key < root.Left.Key)
+                {
+                    return this.RightRotate(ref root);
+                }
+
+            }
+            else if (balance_factor > 1)
+            {
+                // right leaning
+            }
+
+
+            Console.WriteLine(balance_factor);
+
+            return new Node(null, null, 45, 55, false);
+        }
+
+        public Node RightRotate(ref Node root)
+        {
+
+            return;
+        }
+
+        public void Remove(ref Node root, Node node_to_remove)
+        {
+            WriteNodeToFile("", node_to_remove);
+            return;
+        }
+
+        public long Search(long key, Node root)
+        {
+
+            // we need to do some traversal; 
+
+            while (root != null)
+            {
+
+                if (root.Equals(key))
+                {
+                    return key;
+                }
+            }
+
+            // perform searching algorithm
+            return -1;
+        }
+
+
+        public void Display(Node root)
+        {
+        }
+
+
+        // Write each node that is the size of a block to filepath
+        public void WriteNodeToFile(string filePath, Node node)
+        {
+            File.WriteAllText(filePath, "");
+            return;
+        }
+
+        public List<Node> ReadNodes(string filePath)
+        {
+
+            string readText = File.ReadAllText(filePath);
+
+            // TODO: Do some processing to convert the string to a list of BTreeNodes
+
+            return new List<Node>();
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+
+
+        }
+
+
+    }
+
+
+}
+
